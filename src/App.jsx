b9f6b00a6b5893bc5ef96e9a5da466de574cd655
marco94gug <marco.guglielmino94@gmail.com>
2022-12-30 16:36:23 +0100

    feat: added comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,27 @@ import BtnCounter from "./components/BtnCounter";
 import Card from "./components/Card";
 
 function App() {
+  // useState per gestire le nostre variabili che mostriamo sul DOM
+  // alla mutazione di una "variabile" stato, il componente si rirenderizzerà mostrando il dato mutato.
+  // si definisce con un value e un setValue, il setValue muta il value
   const [number, setNumber] = useState(0);
   const [title, setTitle] = useState("Sono la Home");
   const [post, setPost] = useState([]);
   const [type, setType] = useState("posts");
 
+  // useEffect per gestire i side effect, un esempio tipico sono le chiamate fetch, oppure gli eventlistener
+  // ha un array di dipendenze, al variare degli stati definiti dentro l'array, si riesegue lo useEffect
+  // con array vuoto, lo useEffect si eseguirà al montaggio del componente/app
+  // lo useEffect ha un return, il return si esegue allo smontaggio del componente/app,
+  // in genere si usa per rimovere gli eventListener creati nello useEffect stesso.
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((res) => res.json())
-      .then((data) => setPost(data.filter((p, i) => i < number)));
+      .then((data) => setPost(data.filter((_, i) => i < number)));
   }, [number, type]);
 
   const increseCounter = () => {
+    // qui mutiamo il value "number" tramite una funzione che ha parametro ha il valore precedente di number
     setNumber((prev) => prev + 1);
   };
 
@@ -31,6 +40,7 @@ function App() {
     <div className="App">
       <h1>{title}</h1>
       <BtnCounter
+        // props di BtnCounter
         text="Increse"
         color="green"
         textColor="white"
@@ -47,7 +57,13 @@ function App() {
       <BtnCounter text="comment" method={() => setType("comments")} />
       <BtnCounter text="posts" method={() => setType("posts")} />
       <ul>
-        {post.map((item, index, array) => (
+        {
+          // mappiamo i risultati della chiamata fetch
+          // il map funziona sempre perchè lo useState di post è stato inizializzato con un array vuoto []
+          // altrimenti si doveva fare un controllo ternario su post
+          // e.g. post.lenght > 0 && post.map
+        }
+        {post.map((item, index) => (
           <Card key={index} body={item.body} title={item.title || item.name} />
         ))}
       </ul>
@@ -56,3 +72,8 @@ function App() {
 }
 
 export default App;
+
+// ESEMPI DI TERNARI:
+//
+// post ?? comment <-------
+// post ? post : comment <------ ternario dell'esempio sopra
